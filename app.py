5e9c721312e12107d3a7369e25804a31b3faba03
Aruna Amaresan <aruna.amaresan@gmail.com>
2018-03-27 04:22:30 -0400

    Crunch Analyze By year plots

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ app = Flask(__name__)
 #################################################
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy import and_
+from sqlalchemy import func
 
 # The database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Data/MovieDB3.sqlite"
@@ -440,6 +442,266 @@ def movie_data_2013():
         "type": "bar"
     }
     return jsonify(plot_trace)
+
+@app.route("/total_revenue")
+def movie_total_revenue_by_year():
+    """Return emoji score and emoji char"""
+    # query for the top 10 revenue movie data
+    '''results = db.session.query(func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(Movie.budget >= 1000000.0).\
+        order_by(Movie.revenue.desc()).\
+        all()'''
+    #Pulling out budget values that are invalid by filtering out greater >= 1million 
+    results_17 = db.session.query(func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%17'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.revenue.desc()).\
+        all()
+
+    results_16 = db.session.query(func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%16'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.revenue.desc()).\
+        all()
+
+    results_15 = db.session.query(func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%15'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.revenue.desc()).\
+        all()
+
+    results_14 = db.session.query(func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%14'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.revenue.desc()).\
+        all()
+
+    results_13 = db.session.query(func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%13'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.revenue.desc()).\
+        all()
+
+    '''print(results)
+
+    print (results_17)
+    print (results_16)
+    print (results_15)
+    print (results_14)
+    print (results_13)'''
+
+    # Select the top 10 query results
+    #year_info = ["2017", "2016", "2015", "2014", "2013"]
+    year_info = ["2013", "2014", "2015", "2016", "2017"]
+    #movie_title = [result[0] for result in results]
+    revenues = [0, 0, 0, 0, 0]
+    '''for result in results:
+        revenues[0] = int(result[0]) '''
+
+    for result_total_2017 in results_17:
+        revenues[4] = int(result_total_2017[0])
+
+    for result_total_2016 in results_16:
+        revenues[3] = int(result_total_2016[0])
+
+    for result_total_2015 in results_15:
+        revenues[2] = int(result_total_2015[0])
+
+    for result_total_2014 in results_14:
+        revenues[1] = int(result_total_2014[0])
+
+    for result_total_2013 in results_13:
+        revenues[0] = int(result_total_2013[0])
+
+    #revenues = [int(result[1]) for result in results]
+    for year, total_rev in zip(year_info, revenues):
+        print(year, total_rev)
+
+    color_by_year = ['rgba(186, 134, 13, 1)', #green
+        'rgba(240, 128, 38, 1)', #purple
+        'rgba(186, 47, 13, 1)', #red
+        'rgba(128, 144, 194, 1)', #orange
+        'rgba(13, 186, 47, 1)'] #brown
+
+    # Generate the plot trace
+    plot_trace = {
+        "marker": { "color": color_by_year },
+        "x": year_info,
+        "y": revenues,
+        "type": "bar"
+    }
+
+    return jsonify(plot_trace)
+
+@app.route("/total_profit")
+def movie_total_profit_by_year():
+    """Return emoji score and emoji char"""
+    # query for the top 10 revenue movie data
+    '''results = db.session.query(func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(Movie.budget >= 1000000.0).\
+        order_by(Movie.revenue.desc()).\
+        all()'''
+
+    #Pulling out budget values that are invalid by filtering out greater >= 1million 
+    results_17 = db.session.query(func.sum(Movie.budget), func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%17'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.budget.desc()).\
+        all()
+
+    results_16 = db.session.query(func.sum(Movie.budget), func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%16'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.budget.desc()).\
+        all()
+
+    results_15 = db.session.query(func.sum(Movie.budget), func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%15'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.budget.desc()).\
+        all()
+
+    results_14 = db.session.query(func.sum(Movie.budget), func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%14'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.budget.desc()).\
+        all()
+
+    results_13 = db.session.query(func.sum(Movie.budget), func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%13'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.budget.desc()).\
+        all()
+
+    '''print(results)
+
+    print (results_17)
+    print (results_16)
+    print (results_15)
+    print (results_14)
+    print (results_13)'''
+
+    # Select the top 10 query results
+    #year_info = ["2017", "2016", "2015", "2014", "2013"]
+    year_info = ["2013", "2014", "2015", "2016", "2017"]
+    #movie_title = [result[0] for result in results]
+    profit_values = [0, 0, 0, 0, 0]
+    '''for result in results:
+        revenues[0] = int(result[0]) '''
+
+    for result_total_2017 in results_17:
+        profit_values[4] = ( int(result_total_2017[1]) - int(result_total_2017[0]) )
+
+    for result_total_2016 in results_16:
+        profit_values[3] = ( int(result_total_2016[1]) - int(result_total_2016[0]) )
+
+    for result_total_2015 in results_15:
+        profit_values[2] = ( int(result_total_2015[1]) - int(result_total_2015[0]) )
+
+    for result_total_2014 in results_14:
+        profit_values[1] = ( int(result_total_2014[1]) - int(result_total_2014[0]) )
+
+    for result_total_2013 in results_13:
+        profit_values[0] = ( int(result_total_2013[1]) - int(result_total_2013[0]) )
+
+    #revenues = [int(result[1]) for result in results]
+    for year, total_profit in zip(year_info, profit_values):
+        print(year, total_profit)
+
+    color_by_year = ['rgba(186, 134, 13, 1)', #green
+        'rgba(240, 128, 38, 1)', #purple
+        'rgba(186, 47, 13, 1)', #red
+        'rgba(128, 144, 194, 1)', #orange
+        'rgba(13, 186, 47, 1)'] #brown
+
+    # Generate the plot trace
+    plot_trace = {
+        "marker": { "color": color_by_year },
+        "x": year_info,
+        "y": profit_values,
+        "type": "bar"
+    }
+
+    return jsonify(plot_trace)
+
+
+@app.route("/total_budget")
+def movie_total_budget_by_year():
+    """Return emoji score and emoji char"""
+    # query for the top 10 revenue movie data
+    '''results = db.session.query(func.sum(Movie.revenue), func.count(Movie.original_title)).\
+        filter(Movie.budget >= 1000000.0).\
+        order_by(Movie.revenue.desc()).\
+        all()'''
+
+    #Pulling out budget values that are invalid by filtering out greater >= 1million 
+    results_17 = db.session.query(func.sum(Movie.budget), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%17'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.budget.desc()).\
+        all()
+
+    results_16 = db.session.query(func.sum(Movie.budget), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%16'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.budget.desc()).\
+        all()
+
+    results_15 = db.session.query(func.sum(Movie.budget), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%15'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.budget.desc()).\
+        all()
+
+    results_14 = db.session.query(func.sum(Movie.budget), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%14'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.budget.desc()).\
+        all()
+
+    results_13 = db.session.query(func.sum(Movie.budget), func.count(Movie.original_title)).\
+        filter(and_(Movie.release_date.like('%13'), Movie.budget >= 1000000.0)).\
+        order_by(Movie.budget.desc()).\
+        all()
+
+    '''print(results)
+
+    print (results_17)
+    print (results_16)
+    print (results_15)
+    print (results_14)
+    print (results_13)'''
+
+    # Select the top 10 query results
+    #year_info = ["2017", "2016", "2015", "2014", "2013"]
+    year_info = ["2013", "2014", "2015", "2016", "2017"]
+    #movie_title = [result[0] for result in results]
+    budget_values = [0, 0, 0, 0, 0]
+    '''for result in results:
+        revenues[0] = int(result[0]) '''
+
+    for result_total_2017 in results_17:
+        budget_values[4] = int(result_total_2017[0])
+
+    for result_total_2016 in results_16:
+        budget_values[3] = int(result_total_2016[0])
+
+    for result_total_2015 in results_15:
+        budget_values[2] = int(result_total_2015[0])
+
+    for result_total_2014 in results_14:
+        budget_values[1] = int(result_total_2014[0])
+
+    for result_total_2013 in results_13:
+        budget_values[0] = int(result_total_2013[0])
+
+    #revenues = [int(result[1]) for result in results]
+    for year, total_budget in zip(year_info, budget_values):
+        print(year, total_budget)
+
+    color_by_year = ['rgba(186, 134, 13, 1)', #green
+        'rgba(240, 128, 38, 1)', #purple
+        'rgba(186, 47, 13, 1)', #red
+        'rgba(128, 144, 194, 1)', #orange
+        'rgba(13, 186, 47, 1)'] #brown
+
+    # Generate the plot trace
+    plot_trace = {
+        "marker": { "color": color_by_year },
+        "x": year_info,
+        "y": budget_values,
+        "type": "bar"
+    }
+
+    return jsonify(plot_trace)
+
+
 
 '''
 @app.route("/scatter_plot_test")
